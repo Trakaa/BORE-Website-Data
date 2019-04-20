655982c12b580758813110a16a067e43709e1570
Steve <trakaa@gmail.com>
2019-04-20 17:44:15 -0400

    Create UpdateWalletJournal

---
+++ UpdateWalletJournal.php
@@ -0,0 +1,63 @@
+<?php
+
+//Loop through each wallet Division 1-7
+for ($i = 1; $i <= 7; $i++) {
+	// Lets get the wallet data with a GET
+	$remote_url = "https://esi.evetech.net/v4/corporations/853746728/wallets/$i/journal";
+
+	$opts = array(
+	  'http' => array(
+	    'method' => 'GET',
+	    'header' => array(
+	         "User-Agent: ...",
+	         "Authorization: Bearer $accesstoken ",
+	         "Host: esi.evetech.net"
+	    ),
+	  )
+	);
+
+	$context = stream_context_create($opts);
+
+	// Parse the results into an array
+	$result = file_get_contents($remote_url, false, $context);
+
+	$walletjournaldata = json_decode($result, true);
+
+	foreach ($walletjournaldata as $row) {
+
+		//Amount
+		$amount = $row['amount'];
+
+		//Balance
+		$balance = $row['balance'];
+
+		//Date
+		$date = new DateTime($row['date']);
+		$date = $date->format('Y-m-d H:i');
+
+		//Description
+		$description = mysqli_escape_string($conn, $row['description']);
+
+		//First Party ID
+		$firstpartyid = mysqli_escape_string($conn, $row['first_party_id']);
+
+		//ID
+		$id = $row['id'];
+
+		//Ref Type
+		$reftype = mysqli_escape_string($conn, $row['ref_type']);
+
+		//Second Party ID
+		$secondpartyid = mysqli_escape_string($conn, $row['second_party_id']);
+
+		//Write the Notif table
+		$sql = "INSERT IGNORE INTO data_walletjournal (amount,balance,date,description,firstpartyid,walletjournalid,reftype,secondpartyid) VALUES ($amount,$balance,'$date','$description',$firstpartyid,$id,'$reftype',$secondpartyid)";
+
+		$query = mysqli_query($conn, $sql);
+
+		if (!$query) {
+		    printf("Errormessage: %s\n", mysqli_error($conn));
+		}
+	}
+}
+?>
